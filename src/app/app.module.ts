2b19bc46e2a84f49a84d0f5c4f182b641e6ff8e7
Michael Mallari <michael@michaelmallari.com>
2017-02-23 17:06:35 -0500

    ng generate component src/app/components/newsletter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NewsletterComponent } from './components/newsletter/newsletter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewsletterComponent
   ],
   imports: [
     BrowserModule,
