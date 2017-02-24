1a7ded74eea505d4e54ddb9d84feedc333438368
Michael Mallari <michael@michaelmallari.com>
2017-02-24 07:39:42 -0500

    Updated content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Dog walker for a social pack of Manhattanite pups';
-  description = 'We are a NYC-based dog-walking service that pairs dog care with technology to show that our actions speak louder than words.';
+  description = 'Little Pairings is a NYC-based dog-walking service that pairs our dog care approach with technology. #TrustEarned';
 }
