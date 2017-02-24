215bb605a0399dbb32b6539ac3188bcecf8ef542
Michael Mallari <michael@michaelmallari.com>
2017-02-23 19:50:22 -0500

    Updated HTML structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Little Pairings';
+  title = 'Dog walker for a social pack of Manhattanite pups';
+  description = 'We are a NYC-based dog-walking service that pairs dog care with technology to show that our actions speak louder than words.';
 }
